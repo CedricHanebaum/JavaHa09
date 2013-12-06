d5efb1fcbe062e2143ba9008617c45b88d4aa3a8
CedricHanebaum@cae <Cedric.Hanebaum@cae.de>
2013-12-06 14:13:05 +0100

    some bugfixes

--- zahlenwoerter/ZahlenwortDeutsch.java
+++ zahlenwoerter/ZahlenwortDeutsch.java
@@ -2,10 +2,11 @@ package zahlenwoerter;
 
 public class ZahlenwortDeutsch extends Zahlenwort {
 
-	private String[] einer = { "", "ein", "zwei", "drei", "vier", "fünf", "sechs", "sieben", "acht", "neun" };
-	private String[] zehner = { "", "zehn", "undzwanzig", "unddreißig", "undvierzig", "undfünfzig", "undsechzig", "undsiebzig", "undachtzig", "undneunzig" };
+	private String[] einer = { "", "ein", "zwei", "drei", "vier", "fuenf", "sechs", "sieben", "acht", "neun" };
+	private String[] zehner = { "", "zehn", "zwanzig", "dreissig", "vierzig", "fuenfzig", "sechzig", "siebzig", "achtzig", "neunzig" };
 	private String hundert = "hundert";
 	private String tausend = "tausend";
+	private String und = "und";
 
 	public ZahlenwortDeutsch(int x) {
 		super(x);
@@ -20,23 +21,28 @@ public class ZahlenwortDeutsch extends Zahlenwort {
 		if (Zahlenwort.getDigitAt(x, 100) != 0) {
 			ret += einer[Zahlenwort.getDigitAt(x, 100)] + hundert;
 		}
-		if (Zahlenwort.getDigitAt(x, 1) != 0) {
-			if (Zahlenwort.getDigitAt(x, 10) != 1 || !(Zahlenwort.getDigitAt(x, 1) == 1 || Zahlenwort.getDigitAt(x, 1) == 2)) {
+		if (Zahlenwort.getDigitAt(x, 1) != 0 && Zahlenwort.getDigitAt(x, 10) != 1) {
 				ret += einer[Zahlenwort.getDigitAt(x, 1)];
-			}
 		}
 		if (Zahlenwort.getDigitAt(x, 10) != 0) {
-			if (Zahlenwort.getDigitAt(x, 10) == 1 && (Zahlenwort.getDigitAt(x, 1) == 1 || Zahlenwort.getDigitAt(x, 1) == 2)) {
+			if (Zahlenwort.getDigitAt(x, 10) == 1) {
+				if (Zahlenwort.getDigitAt(x, 1) == 0) ret += "zehn";
 				if (Zahlenwort.getDigitAt(x, 1) == 1) ret += "elf";
-				if (Zahlenwort.getDigitAt(x, 1) == 2) ret += "zwölf";
+				if (Zahlenwort.getDigitAt(x, 1) == 2) ret += "zwoelf";
+				if (Zahlenwort.getDigitAt(x, 1) == 3) ret += "dreizehn";
+				if (Zahlenwort.getDigitAt(x, 1) == 4) ret += "vierzehn";
+				if (Zahlenwort.getDigitAt(x, 1) == 5) ret += "fuenfzehn";
+				if (Zahlenwort.getDigitAt(x, 1) == 6) ret += "sechzehn";
+				if (Zahlenwort.getDigitAt(x, 1) == 7) ret += "siebzehn";
+				if (Zahlenwort.getDigitAt(x, 1) == 8) ret += "achtzehn";
+				if (Zahlenwort.getDigitAt(x, 1) == 9) ret += "neunzehn";
 			} else {
+				if(Zahlenwort.getDigitAt(x, 1) != 0) ret += und;
 				ret += zehner[Zahlenwort.getDigitAt(x, 10)];
 			}
 		}
 
 		if (x == 1) ret = "eins";
-		if (x == 11) ret = "elf";
-		if (x == 12) ret = "zwölf";
 
 		return ret;
 	}
--- zahlenwoerter/ZahlenwortEnglisch.java
+++ zahlenwoerter/ZahlenwortEnglisch.java
@@ -23,18 +23,19 @@ public class ZahlenwortEnglisch extends Zahlenwort {
 		}
 		if (Zahlenwort.getDigitAt(x, 10) != 0) {
 			if (Zahlenwort.getDigitAt(x, 10) == 1) {
-				if (Zahlenwort.getDigitAt(x, 1) == 0) ret += " ten";
-				if (Zahlenwort.getDigitAt(x, 1) == 1) ret += " eleven";
-				if (Zahlenwort.getDigitAt(x, 1) == 2) ret += " twelve";
-				if (Zahlenwort.getDigitAt(x, 1) == 3) ret += " thirteen";
-				if (Zahlenwort.getDigitAt(x, 1) == 4) ret += " fourteen";
-				if (Zahlenwort.getDigitAt(x, 1) == 5) ret += " fifteen";
-				if (Zahlenwort.getDigitAt(x, 1) == 6) ret += " sixteen";
-				if (Zahlenwort.getDigitAt(x, 1) == 7) ret += " seventeen";
-				if (Zahlenwort.getDigitAt(x, 1) == 8) ret += " eighteen";
-				if (Zahlenwort.getDigitAt(x, 1) == 9) ret += " nineteen";
+				if (Zahlenwort.getDigitAt(x, 1) == 0) ret += "ten";
+				if (Zahlenwort.getDigitAt(x, 1) == 1) ret += "eleven";
+				if (Zahlenwort.getDigitAt(x, 1) == 2) ret += "twelve";
+				if (Zahlenwort.getDigitAt(x, 1) == 3) ret += "thirteen";
+				if (Zahlenwort.getDigitAt(x, 1) == 4) ret += "fourteen";
+				if (Zahlenwort.getDigitAt(x, 1) == 5) ret += "fifteen";
+				if (Zahlenwort.getDigitAt(x, 1) == 6) ret += "sixteen";
+				if (Zahlenwort.getDigitAt(x, 1) == 7) ret += "seventeen";
+				if (Zahlenwort.getDigitAt(x, 1) == 8) ret += "eighteen";
+				if (Zahlenwort.getDigitAt(x, 1) == 9) ret += "nineteen";
 			} else {
-				ret += " " + zehner[Zahlenwort.getDigitAt(x, 10)];
+				if(Zahlenwort.getDigitAt(x, 100) != 0 || Zahlenwort.getDigitAt(x, 1000) != 0) ret += " ";
+				ret += zehner[Zahlenwort.getDigitAt(x, 10)];
 			}
 		}
 		if (Zahlenwort.getDigitAt(x, 10) != 1) {
@@ -45,8 +46,9 @@ public class ZahlenwortEnglisch extends Zahlenwort {
 			}
 			ret += einer[Zahlenwort.getDigitAt(x, 1)];
 		}
-
-		ret = ret.substring(0, ret.lastIndexOf(" ")) + " " + und + ret.substring(ret.lastIndexOf(" "));
+		if(ret.contains(" ")){
+			ret = ret.substring(0, ret.lastIndexOf(" ")) + " " + und + ret.substring(ret.lastIndexOf(" "));
+		}
 
 		return ret;
 	}
